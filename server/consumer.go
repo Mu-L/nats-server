950a61f390982fff0e319c331b5cf00a8883f769
Maurice van Veen <github@mauricevanveen.com>
2026-06-16 10:32:45 +0200

    (2.14) [IMPROVED] Consumer single-replica consistency
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/consumer.go
+++ server/consumer.go
@@ -2866,13 +2866,11 @@ VALID:
 				o.mu.Lock()
 			}
 		}
-
-		// Recalculate pending, and re-trigger message delivery.
-		o.streamNumPending()
-		o.signalNewMessages()
-		return seq, true, nil
 	}
-	return seq, false, nil
+	// Recalculate pending, and re-trigger message delivery.
+	o.streamNumPending()
+	o.signalNewMessages()
+	return seq, true, nil
 }
 
 // Lock should be held.
